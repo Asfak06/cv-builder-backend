9d97cf74d7f42bcc6c15699c0b4260b932b33262
asfak06 <asfakmunna@gmail.com>
2025-02-18 15:25:16 +0600

    intial prod

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const connectDB = require("./src/config/database");
 // Connect to MongoDB
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4004;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
